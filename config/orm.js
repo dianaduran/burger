ec24de28c831d54ae55ff0db740156f62daba6fe
Diana Duran <dianaduran17@gmail.com>
2018-05-06 17:57:37 -0400

    to heroku

--- config/orm.js
+++ config/orm.js
@@ -57,7 +57,7 @@ var orm = {
         queryString += "?";
         queryString += ") ";
 
-        console.log(queryString);
+        //console.log(queryString);
 
         connection.query(queryString, vals, function(err, result) {
             if (err) {
@@ -77,7 +77,7 @@ var orm = {
         queryString += " WHERE ";
         queryString += condition;
 
-        console.log(queryString);
+        // console.log(queryString);
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
